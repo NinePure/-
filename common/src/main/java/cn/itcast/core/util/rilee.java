fb46ba7d2bcb36ff19f334a83a778e97f4611625
tankan <tankan@aliyun.com>
2019-01-09 14:31:11 +0800

    tankan test 6

--- common/src/main/java/cn/itcast/core/util/rilee.java
+++ common/src/main/java/cn/itcast/core/util/rilee.java
@@ -2,6 +2,6 @@ package cn.itcast.core.util;
 
 public class rilee {
     public static void main(String[] args) {
-        System.out.println(123456);
+        System.out.println(1234526);
     }
 }
