989b47c6cd97baf47e082d8ac7c46e51840bc152
liang <zunhua5201314@163.com>
2019-01-11 09:07:19 +0800

    liang 2019111

--- web_user/src/main/java/cn/itcast/core/service/UserDetailServiceImpl.java
+++ web_user/src/main/java/cn/itcast/core/service/UserDetailServiceImpl.java
@@ -1,5 +1,6 @@
 package cn.itcast.core.service;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -17,10 +18,29 @@ import java.util.List;
  * 如果能进入到这个实现类, 说明cas已经认证通过, 这里只做赋权操作
  */
 public class UserDetailServiceImpl implements UserDetailsService {
+
+    private ManageUserService manageUserService;
+
+    public void setManageUserService(ManageUserService manageUserService) {
+        this.manageUserService = manageUserService;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
         //定义权限集合
         List<GrantedAuthority> authList = new ArrayList<>();
+
+        List<cn.itcast.core.pojo.user.User> userList = manageUserService.updateAuthority();
+        if (userList!=null){
+            for (cn.itcast.core.pojo.user.User user : userList) {
+                if (username.equals(user.getUsername())){
+                    //向权限集合中加入访问权限
+                    authList.add(new SimpleGrantedAuthority("ROLE_NO"));
+                    return new User(username, "", authList);
+                }
+            }
+        }
         //向权限集合中加入访问权限
         authList.add(new SimpleGrantedAuthority("ROLE_USER"));
         return new User(username, "", authList);
