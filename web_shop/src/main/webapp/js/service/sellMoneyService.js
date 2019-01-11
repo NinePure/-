848153c9e8bdbde8fa8f19dfe73f52f85007208f
chongyang <1486787536@qq.com>
2019-01-11 10:27:35 +0800

    重阳2次修改

--- web_shop/src/main/webapp/js/service/sellMoneyService.js
+++ web_shop/src/main/webapp/js/service/sellMoneyService.js
@@ -1,9 +1,9 @@
 //服务层
 app.service('sellMoneyService',function($http){
-	    	
 
-	//搜索
-	this.search=function(page,rows,startTime,endTime){
-		return $http.post('../order/findSellMoney.do?page='+page+"&rows="+rows+"&startTime="+startTime+"&endTime="+endTime);
-	}    	
+
+    //搜索
+    this.search=function(page,rows,startTime,endTime){
+        return $http.post('../order/findSellMoney.do?page='+page+"&rows="+rows+"&startTime="+startTime+"&endTime="+endTime);
+    }
 });
